fe475f6f3f3d609bae838fb3cdac735d561bc7f0
PythonGermany <pythongermany@gmail.com>
2022-04-06 20:52:38 +0200

    Performance/Reliability improved

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Drawing;
 
 namespace amongUsFinder
@@ -11,124 +8,140 @@ namespace amongUsFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username: ");
-            string username = Console.ReadLine();
-            Console.WriteLine("Enter filename: ");
-            var filePath = @"C:\Users\" + username + @"\Downloads\" + Console.ReadLine();
+            //Console.WriteLine("Enter username: ");
+            //string username = Console.ReadLine();
+            string username = "pythongermany";
+            //Console.WriteLine("Enter filename: ");
+            //var filePath = @"C:\Users\" + username + @"\Downloads\" + Console.ReadLine();
+            var filePath = @"C:\Users\pythongermany\Downloads\040420222340.png";
+            int numberFound = 0;
             Bitmap place = new Bitmap(filePath);
             Bitmap bmp = new Bitmap(filePath);
-            List<Color> border;
-            int[,] amongus = new int[5, 4] { { 0, 1, 1, 1},
-                                             { 1, 1, 2, 2},
-                                             { 1, 1, 1, 1},
-                                             { 3, 1, 3, 1},
+            bool end = false;
+            int[,] amongus = new int[5, 4] { { 0, 2, 2, 2},
+                                             { 2, 2, 1, 1},
+                                             { 2, 2, 2, 2},
+                                             { 3, 2, 3, 2},
                                              { 0, 3, 0, 3}};
             for (int y = 0; y < bmp.Height - 1; y++)
             {
                 for (int x = 0; x < bmp.Width; x++)
                 {
                     Color c = bmp.GetPixel(x, y);
-                    Color cNew = Color.FromArgb((int)(c.A * 0.2), c.R, c.G, c.B);
+                    int bw = (int)Math.Round((double)(c.R + c.G + c.B) / 3, 0);
+                    Color cNew = Color.FromArgb((int)(0.75 * c.A), bw, bw, bw);
                     bmp.SetPixel(x, y, cNew);
                 }
             }
-            int numberFound = 0;
-            bool search;
-            bool mirror = false;
-            for (int y = 0; y <= place.Height - 5; y++)
-            {
-                for (int x = 0; x <= place.Width - 4 - 1; x++)
-                {
-                    for (int i = 0; i < 2; i++)
-                    {
-                        search = true;
-                        if (i == 0) mirror = false;
-                        else if (i == 1) mirror = true;
-                        Color c1 = place.GetPixel(tC(x, -0.5), y);
-                        Color c2 = place.GetPixel(tC(x, 0.5), y + 1);
-                        if (c2 != place.GetPixel(tC(x, 1.5), y + 1) || c2 == c1) search = false;
-                        if (search)
-                        {
-                            for (int row = 0; row < 5; row++)
-                            {
-                                for (int column = 0; column < 4; column++)
-                                {
-                                    if (amongus[row, column] == 1 && c1 != place.GetPixel(tC(x, -1.5 + column), y + row))
-                                    {
-                                        search = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        if (search)
-                        {
-                            border = new List<Color>()
-                        {
-                            //place.GetPixel(tC(x, -1.5), y),
-                            //place.GetPixel(tC(x, -1.5), y + 3),
-                            //place.GetPixel(tC(x, -1.5), y + 4),
-                            place.GetPixel(tC(x, 0.5), y + 4)
-                            };
-                            if (x > 0 && !mirror || x < place.Width - 4 && mirror)
-                            {
-                                //border.Add(place.GetPixel(tC(x, -2.5), y + 1));
-                                //border.Add(place.GetPixel(tC(x, -2.5), y + 2));
-                            }
-                            if (y > 0)
-                            {
-                                //border.Add(place.GetPixel(tC(x, -0.5), y - 1));
-                                //border.Add(place.GetPixel(tC(x, 0.5), y - 1));
-                                //border.Add(place.GetPixel(tC(x, 1.5), y - 1));
-                            }
-                            if (x < place.Width - 4 && !mirror || x > 0 && mirror)
-                            {
-                                //border.Add(place.GetPixel(tC(x, 2.5), y));
-                                border.Add(place.GetPixel(tC(x, 2.5), y + 2));
-                                //border.Add(place.GetPixel(tC(x, 2.5), y + 3));
-                                //border.Add(place.GetPixel(tC(x, 2.5), y + 4));
+            Thread t = new Thread(() => searchAmongus(0, 0));
+            t.Start();
 
-                            }
-                            if (border.Contains(c1))
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if (end)
+                {
+                    bmp.Save(@"C:\Users\" + username + @"\Downloads\test.png", System.Drawing.Imaging.ImageFormat.Png);
+                    Console.WriteLine($"0|2000 --> {numberFound} found");
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+            void searchAmongus(int xStart, int yStart)
+            {
+                int xStop = 2000, yStop = 2000;
+                if (xStart > 3)
+                {
+                    xStart -= 3;
+                    xStop += 3;
+                }
+                if(yStart > 3)
+                {
+                    yStart -= 3;
+                    yStop += 3;
+                }
+                for (int y = yStart; y < yStart + yStop - 4; y++)
+                {
+                    for (int x = xStart; x < xStart + xStop - 3; x++)
+                    {
+                        for (int i = 0; i < 2; i++)
+                        {
+                            bool mirror = false;
+                            bool search = true;
+                            if (i == 0) mirror = false;
+                            else if (i == 1) mirror = true;
+                            Color c1 = place.GetPixel(tC(x, -0.5, mirror), y);
+                            Color c2 = place.GetPixel(tC(x, 0.5, mirror), y + 1);
+                            if (c2 != place.GetPixel(tC(x, 1.5, mirror), y + 1) || c2 == c1) search = false;
+                            if (search)
                             {
-                                search = false;
-                            }
-                            else
-                            {
-                                for (int row = 0; row < 5; row++)
+                                int priority = 2;
+                                int border = 0;
+                                for (int p = priority; p >= 0; p--)
                                 {
-                                    for (int column = 0; column < 4; column++)
+                                    for (int row = 0; row < 5; row++)
                                     {
-                                        if (amongus[row, column] == 1)
+                                        for (int column = 0; column < 4; column++)
                                         {
-                                            bmp.SetPixel(tC(x, column - 1.5), y + row, c1);
-                                        }
-                                        else if (amongus[row, column] == 2)
-                                        {
-                                            bmp.SetPixel(tC(x, column - 1.5), y + row, c2);
-                                        }
-                                        else if (amongus[row, column] == 3 && c1 == place.GetPixel(tC(x, column - 1.5), y + row))
-                                        {
-                                            bmp.SetPixel(tC(x, column - 1.5), y + row, c1);
+                                            if (amongus[row, column] == 2 && c1 != place.GetPixel(tC(x, -1.5 + column, mirror), y + row))
+                                            {
+                                                search = false;
+                                                break;
+                                            }
+                                            else if (amongus[row, column] == 0 && c1 == place.GetPixel(tC(x, -1.5 + column, mirror), y + row))
+                                            {
+                                                border++;
+                                            }
                                         }
                                     }
                                 }
+                                if (border >= 3)
+                                {
+                                    search = false;
+                                    break;
+                                }
+                            }
+                            if (search)
+                            {
+                                drawAmongus(x, y, mirror, c1, c2);
                                 numberFound++;
+                                x += 3;
                                 i = 2;
                             }
                         }
+                        if (x == 0 && y % 100 == 0)
+                        {
+                            Console.WriteLine($"{x}|{y} --> {numberFound} found"); 
+                        }
                     }
-                    Console.WriteLine($"{x.ToString("0000")}|{y.ToString("0000")} -- > {numberFound} found");
                 }
+                end = true;
             }
-            bmp.Save(@"C:\Users\" + username + @"\Downloads\test.png", System.Drawing.Imaging.ImageFormat.Png);
-            Console.ReadKey();
 
-            int tC(int x, double shift)
+            int tC(int xC, double shift, bool mirror)
             {
                 int f = 1;
                 if (mirror) f = -1;
-                return (int)(x + 1.5 + shift * f);
+                return (int)(xC + 1.5 + shift * f);
+            }
+
+            void drawAmongus(int xS, int yS, bool mirror, Color c1S, Color c2S)
+            {
+                for (int row = 0; row < 5; row++)
+                {
+                    for (int column = 0; column < 4; column++)
+                    {
+                        if (amongus[row, column] >= 2 && c1S == place.GetPixel(tC(xS, column - 1.5, mirror), yS + row))
+                        {
+                            bmp.SetPixel(tC(xS, column - 1.5, mirror), yS + row, c1S);
+                        }
+                        else if (amongus[row, column] == 1)
+                        {
+                            bmp.SetPixel(tC(xS, column - 1.5, mirror), yS + row, c2S);
+                        }
+                    }
+                }
             }
         }
     }
